d4661079b12eeed73581c79954c44ebd07ab5760
Gagandeep Singh <gagandeep161198@gmail.com>
2025-07-16 20:02:33 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "gagandeep-portfolio",
+  base: "/Gagandeep-portfolio/",
   plugins: [react(), tailwindcss()],
 })
